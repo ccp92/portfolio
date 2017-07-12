cc839a5173336487dca2b10b514ae4d35da5ad42
Chris <mail@christopherparsons.me>
2017-07-12 18:55:37 +0100

    Animation
    ~Animated change between greyscale and colour on rollover

--- port.js
+++ port.js
@@ -1,9 +1,25 @@
-function onHover()
-{
-    $("#card1").attr('src', 'assets/test/quad2.jpeg');
-}
-
-function offHover()
-{
-    $("#car1").attr('src', 'assets/test/quad1.jpeg');
-}
+$(document).ready(function() {
+	$('#card1').hover(function() {
+		$(this).attr('src', 'assets/test/quad1.jpeg');
+	}, function() {
+		$(this).attr('src', 'assets/test/quad1bw.jpeg');
+	});
+	
+	$('#card2').hover(function() {
+		$(this).attr('src', 'assets/test/quad2.jpeg');
+	}, function() {
+		$(this).attr('src', 'assets/test/quad2bw.jpeg');
+	});
+	
+	$('#card3').hover(function() {
+		$(this).attr('src', 'assets/test/quad3.jpeg');
+	}, function() {
+		$(this).attr('src', 'assets/test/quad3bw.jpeg');
+	});
+	
+	$('#card4').hover(function() {
+		$(this).attr('src', 'assets/test/quad4.jpeg');
+	}, function() {
+		$(this).attr('src', 'assets/test/quad4bw.jpeg');
+	});
+});
