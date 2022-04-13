4b2f20c471c2ba1033019c209d8fdac4577197a0
ccp92 <mail@chrisparsons.dev>
2022-04-13 23:03:42 +0100

    Copy over existing code

---
+++ src/components/intro/index.tsx
@@ -0,0 +1,30 @@
+const Intro = (): JSX.Element => {
+  return (
+    <div className="intro">
+      <div>
+        <p className="header-text">
+          Hey there, I'm <span className="dev-name">Chris Parsons</span>, a
+          Software Engineer.
+        </p>
+        <div className="button-row">
+          <a
+            className="action-button main-action-button"
+            data-test="main-action-button"
+            href="https://github.com/ccp92"
+          >
+            Github profile
+          </a>
+          <a
+            className="action-button"
+            data-test="action-button"
+            href={`mailto:${process.env.REACT_APP_EMAIL_ADDRESS}`}
+          >
+            Contact Me
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
---
+++ src/components/timeline/index.tsx
@@ -0,0 +1,26 @@
+const Timeline = (): JSX.Element | null => {
+  return process.env.REACT_APP_FEATURE_TIMELINE ? (
+    <div className="timeline">
+      <h1>Timeline</h1>
+      <div className="job-card">
+        <h2 className="company">Babylon Health</h2>
+        <h3 className="position">Software Engineer</h3>
+        <p className="dates">January 2022 - Present</p>
+      </div>
+
+      <div className="job-card">
+        <h2 className="company">Made Tech</h2>
+        <h3 className="position">Software Engineer</h3>
+        <p className="dates">June 2018 - December 2021</p>
+      </div>
+
+      <div className="job-card">
+        <h2 className="company">Real Asset Management</h2>
+        <h3 className="position">Technical Support Consultant</h3>
+        <p className="dates">November 2013 - March 2018</p>
+      </div>
+    </div>
+  ) : null;
+};
+
+export default Timeline;
