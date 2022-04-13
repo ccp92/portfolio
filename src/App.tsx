d07e6b6c810dc5ae24b1bfc79a654ef71ad6f38c
ccp92 <mail@chrisparsons.dev>
2022-04-13 23:26:45 +0100

    Add css to split screen into two columns

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,28 @@ import Intro from "./components/intro";
 import Timeline from "./components/timeline";
 
 const App = () => {
+  const featureFlagOff = () => {
+    return <Intro />;
+  };
+
+  const featureFlagOn = () => {
+    return (
+      <div className="row">
+        <div className="column">
+          <Intro />
+        </div>
+        <div className="column">
+          <Timeline />
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="App">
-      <Intro />
-      <Timeline />
+      {process.env.REACT_APP_FEATURE_TIMELINE === "true"
+        ? featureFlagOn()
+        : featureFlagOff()}
     </div>
   );
 };
