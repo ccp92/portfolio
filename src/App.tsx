3ddf160e0e4c9db262e47f3236e96457057df8fd
ccp92 <mail@chrisparsons.dev>
2022-05-10 03:16:04 +0100

    Add check for system preference for theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,57 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Intro from "./components/intro";
 import ThemeButton from "./components/themeButton";
 import Timeline from "./components/timeline";
 
 const App = () => {
-  const [theme, setTheme] = useState("dark");
+  const DEFAULT_COLOUR_SCHEME = "dark";
+  const [theme, setTheme] = useState(DEFAULT_COLOUR_SCHEME);
+
+  const useThemeDetector = () => {
+    const currentTheme = () =>
+      window.matchMedia("(prefers-color-scheme: light)").matches;
+    const [isLightTheme, setIsLightTheme] = useState(currentTheme());
+
+    const mqListener = (e: any) => {
+      setIsLightTheme(e.matches);
+    };
+
+    useEffect(() => {
+      const lightThemeMq = window.matchMedia("(prefers-color-scheme: light)");
+      lightThemeMq.addEventListener("change", () => mqListener);
+      return () => lightThemeMq.removeEventListener("change", () => mqListener);
+    }, []);
+    console.log(isLightTheme);
+    return isLightTheme;
+  };
+
+  const isLightTheme = useThemeDetector();
+
+  useEffect(() => {
+    if (isLightTheme) {
+      setTheme("light");
+    } else {
+      setTheme("dark");
+    }
+  }, [isLightTheme]);
 
   const toggleTheme = () => {
     setTheme(theme === "light" ? "dark" : "light");
     console.log(theme);
   };
 
-  const featureFlagOff = () => {
-    return (
-      <>
-        <ThemeButton toggleTheme={toggleTheme} theme={theme} />
-        <Intro />
-      </>
-    );
-  };
-
-  const featureFlagOn = () => {
-    return (
+  return (
+    <div className="App" data-theme={theme}>
       <div className="row">
         <div className="column-left">
-          <ThemeButton toggleTheme={toggleTheme} theme={theme} />
+          <ThemeButton toggleTheme={() => toggleTheme()} theme={theme} />
           <Intro />
         </div>
         <div className="column-right">
           <Timeline />
         </div>
       </div>
-    );
-  };
-
-  return (
-    <div className="App" data-theme={theme}>
-      {process.env.REACT_APP_FEATURE_TIMELINE === "true"
-        ? featureFlagOn()
-        : featureFlagOff()}
     </div>
   );
 };
