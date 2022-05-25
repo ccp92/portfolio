2be8b1f0342e83a02d68dc89f2b79c81b8dafb0f
ccp92 <mail@chrisparsons.dev>
2022-05-25 12:04:00 +0100

    Refactor jobs cards to seperate display and data

--- src/components/timeline/index.tsx
+++ src/components/timeline/index.tsx
@@ -1,3 +1,6 @@
+import JobCard from "../jobCard";
+import jobs from "../../data/jobHistory.json";
+
 const upArrow = () => {
   return (
     <svg
@@ -13,106 +16,23 @@ const upArrow = () => {
   );
 };
 
-const Timeline = (): JSX.Element | null => {
-  return (
-    <div className="timeline">
-      <h1>Experience</h1>
-      <div className="job-card">
-        <h2 className="company">Babylon Health</h2>
-        <h3 className="position">Software Engineer</h3>
-        <p className="dates">January 2022 - Present</p>
-        <p>
-          My current position at Babylon Health as part of the web-platform team
-          involves building tools and procedures for product teams to deliver
-          features quickly to meet user needs.
-        </p>
-        <p>
-          This work involves getting involved in a wide variety of work; from
-          creating typescript packages, to designing guidelines, to building
-          development pipelines.
-        </p>
-        <p>
-          A large part of the work I do on building these packages is ensuring
-          that they are not only functional and meet the needs of the various
-          teams within the organisation, but that they are also flexible and
-          maintainable. By building in this manner, as new requirements arise or
-          prior requirements change, the cost for implementing these changes is
-          low.
-        </p>
-        <p>
-          In addition to this, I help to design and write out guidelines for
-          other developers to work with, ensuring a common development flow
-          across teams when working with our platform.
-        </p>
-      </div>
+const Timeline = () => {
+  const jobsCards = jobs.map((job: any, index: number) => {
+    return (
+      <>
+        <JobCard
+          key={index}
+          company={job.company}
+          position={job.position}
+          dates={job.dates}
+          description={job.description}
+        />
+        {index !== jobs.length - 1 ? upArrow() : null}
+      </>
+    );
+  });
 
-      {upArrow()}
-
-      <div className="job-card">
-        <h2 className="company">Made Tech</h2>
-        <h3 className="position">Software Engineer</h3>
-        <p className="dates">June 2018 - December 2021</p>
-        <p>
-          I initially joined Made Tech via their academy and in my time at the
-          company progressed to being a Software Engineer. My work at Made Tech
-          as a consultant for public sector clients gave me strong skills as a
-          generalist, helping me to jump into existing codebases and hit the
-          ground running.
-        </p>
-        <p>
-          The projects I worked on have varied from greenfield projects such as
-          improvements in the collation of contractual data from local councils,
-          to migrating legacy systems to cloud platforms, to working on large
-          scale internal CRM systems. This work was carried out in an agile way,
-          and pairing and mobbing were common aspects of my daily workflow.
-        </p>
-        <p>
-          Whilst being a consultant required me to pick up multiple tech stacks
-          based upon client needs, I am particularly strong with javascript,
-          typescript, react and express as a tech stack.
-        </p>
-        <p>
-          The consultancy side of my role also gave me strong skills in dealing
-          with end-users and stakeholders, allowing me to parse large amounts of
-          information and requirements into clear goals. I was then able to
-          communicate this effectively back to clients and deal with concerns
-          and queries in a professional manner.
-        </p>
-      </div>
-
-      {upArrow()}
-
-      <div className="job-card">
-        <h2 className="company">Real Asset Management</h2>
-        <h3 className="position">Technical Support Consultant</h3>
-        <p className="dates">November 2013 - March 2018</p>
-        <p>
-          My main role of supporting clients over the phone and email enabled me
-          to cultivate my ability to communicate effectively and with a
-          professional demeanour. In addition, the unscripted support required
-          me to be able to adapt quickly to varying queries and problems, whilst
-          also being able to both understand a query and deliver concise and
-          clearly understandable responses promptly. I also went on-site with
-          clients and provided training and other supplementary services.
-        </p>
-        <p>
-          In addition to my main support duties at the company, I also expanded
-          my responsibilities to other areas. An example of this was regularly
-          being involved in the QA process for the software and the accurate
-          logging of issues for developers to be able to review.
-        </p>
-        <p>
-          Within my first year at the company, I also gained a secondary role of
-          looking after documentation and help files for the software suite. The
-          process for this exposed me to both documentation writing and
-          publishing. I completed this through the use of Adobe RoboHelp to
-          create both chm and html files. I managed these additional
-          responsibilities without impacting my core role of support through
-          appropriate time management and prioritisation of tasks.
-        </p>
-      </div>
-    </div>
-  );
+  return <div className="timeline">{jobsCards}</div>;
 };
 
 export default Timeline;
